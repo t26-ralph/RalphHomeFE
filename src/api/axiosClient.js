2012c3284edf57e40ed8010b48febd3c732c79ee
t26-ralph <152942498+t26-ralph@users.noreply.github.com>
2026-01-21 17:04:35 +0700

    Update axiosClient.js

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: "http://localhost:3000/api", // backend URL
+    baseURL: "https://ralphhomeapi.onrender.com", // backend URL
     headers: {
         "Content-Type": "application/json",
     },
